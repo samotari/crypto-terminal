1dd1ad1d93400baa27db93780858e117a6b88694
BK <boro.kovac@gmail.com>
2017-07-14 21:18:06 +0200

    removed extra comments

--- js/views/payment-confirmation.js
+++ js/views/payment-confirmation.js
@@ -25,12 +25,7 @@ app.views.PaymentConfirmation = (function() {
 
 			var html = $(this.template).html();
 			var template = Handlebars.compile(html);
-			// var data = {};
-			// data.paymentMethods = _.map(app.settings.get('acceptCryptoCurrencies'), function(key) {
-			// 	return { key: key };
-			// });
 			this.$el.html(template());
-			// this.$error = this.$('.error');
 			return this;
 		},
 
@@ -39,29 +34,6 @@ app.views.PaymentConfirmation = (function() {
 			// Navigate back to the homescreen
 			app.router.navigate('main', { trigger: true });
 		},
-
-		// continueWithChosenPaymentMethod: function(evt) {
-
-		// 	evt.preventDefault();
-
-		// 	this.clearError();
-
-		// 	var amount = this.options.amount.toString();
-		// 	var method = $(evt.target).attr('data-payment-method');
-
-		// 	// Navigate to the next screen with the amount in the URI.
-		// 	app.router.navigate('pay/' + encodeURIComponent(amount) + '/' + encodeURIComponent(method), { trigger: true });
-		// // },
-
-		// clearError: function() {
-
-		// 	this.$error.text('');
-		// },
-
-		// showError: function(error) {
-
-		// 	this.$error.text(error);
-		// }
 
 	});
 
