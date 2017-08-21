430cf26cb57ba61aad2646126f8da52c09551948
Victor Brender <victor.brender@gmail.com>
2017-08-21 19:20:23 +0200

    Add dependencies to Cordova project

--- cordova/www/js/index.js
+++ cordova/www/js/index.js
@@ -27,6 +27,33 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
+
+        var _ = require('underscore');
+        var async = require('async');
+        var browserify = require('browserify');
+
+        grunt.registerMultiTask('browserify', function() {
+
+            var done = this.async();
+            var options = this.options();
+
+            async.each(this.files, function(file, next) {
+
+                var srcFiles = grunt.file.expand(file.src);
+                var b = browserify(srcFiles, options);
+
+                b.bundle(function(error, buffer) {
+
+                    if (error) {
+                        return next(error);
+                    }
+
+                    grunt.file.write(file.dest, buffer.toString());
+                    next();
+                });
+
+            }, done);
+        });
         this.receivedEvent('deviceready');
     },
 
