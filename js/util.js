5bfca21e68e07f3247bbd212b5e290a899cc9ca6
Charles Hill <chill@degreesofzero.com>
2017-08-21 19:30:59 +0200

    generatePaymentRequest

--- js/util.js
+++ js/util.js
@@ -5,6 +5,17 @@ app.util = (function() {
 	'use strict';
 
 	return {
+		generateRandomString: function(length, charset) {
+			var randString = '';;
+			charset = charset || 'abcdefghijklmnopqrstuvqxyzABCDEFGHIJKLMNOPQRSTUVQXYZ1234567890';
+			if (typeof charset === 'string') {
+				charset = charset.split('');
+			}
+			while (randString.length < length) {
+				randString += charset[ Math.floor(Math.random() * charset.length) ];
+			}
+			return randString;
+		},
 		extend: function() {
 			var args = Array.prototype.slice.call(arguments);
 			return _.extend.apply(_, [{}].concat(args));
