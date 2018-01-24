0b807a9cb9f3ab56e4a605aaeb54f8eb64b1f0d2
Carlos Garcia Ortiz <karlgoso@gmail.com>
2018-01-24 07:37:34 +0100

    adding Done button confirmation payment view

--- js/lang/en.js
+++ js/lang/en.js
@@ -39,6 +39,7 @@ app.lang['en'] = (function() {
 		'payment-details.date': 'Date',
 		'payment-details.amount': 'Amount',
 		'payment-request.data.must-be-object': '"data" must be an object.',
+		'payment-confirmation.done': 'Done'
 	};
 
 })();
