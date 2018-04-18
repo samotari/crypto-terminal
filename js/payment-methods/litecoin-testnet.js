5eb6cc898803a703185d102468d01b2e1567260f
Carlos Garcia Ortiz <karlgoso@gmail.com>
2018-04-18 19:05:55 +0200

    Commented due to lack of reliable insight API instances

--- js/payment-methods/litecoin-testnet.js
+++ js/payment-methods/litecoin-testnet.js
@@ -1,74 +1,76 @@
-var app = app || {};
+// Commented due to lack of reliable insight API instances.
 
-app.paymentMethods = app.paymentMethods || {};
+// var app = app || {};
 
-app.paymentMethods.litecoinTestnet = (function() {
+// app.paymentMethods = app.paymentMethods || {};
 
-	'use strict';
+// app.paymentMethods.litecoinTestnet = (function() {
 
-	return app.paymentMethods.litecoin.extend({
+// 	'use strict';
 
-		// The name of the cryptocurrency shown in the UI:
-		label: 'Litecoin (testnet)',
+// 	return app.paymentMethods.litecoin.extend({
 
-		// The exchange symbol:
-		code: 'LTC',
+// 		// The name of the cryptocurrency shown in the UI:
+// 		label: 'Litecoin (testnet)',
 
-		// Used internally to reference itself:
-		ref: 'litecoinTestnet',
+// 		// The exchange symbol:
+// 		code: 'LTC',
 
-		// Used for chain.so API requests:
-		chainSoCode: 'LTCTEST',
+// 		// Used internally to reference itself:
+// 		ref: 'litecoinTestnet',
 
-		/*
-			Litecoin testnet network constants.
+// 		// Used for chain.so API requests:
+// 		chainSoCode: 'LTCTEST',
 
-				- Public key hash:
-					Used in the generation of addresses from public keys.
-					(testnet) https://github.com/litecoin-project/litecore-lib/blob/9c3b2712de14335d2a953a8772aee87e23be6cf6/lib/networks.js#L158
+// 		/*
+// 			Litecoin testnet network constants.
 
-				- Script hash:
-					Used in the generation of scripting addresses from public keys.
-					(testnet) https://github.com/litecoin-project/litecore-lib/blob/9c3b2712de14335d2a953a8772aee87e23be6cf6/lib/networks.js#L160
+// 				- Public key hash:
+// 					Used in the generation of addresses from public keys.
+// 					(testnet) https://github.com/litecoin-project/litecore-lib/blob/9c3b2712de14335d2a953a8772aee87e23be6cf6/lib/networks.js#L158
 
-				- WIF:
-					"Wallet Import Format"
-					Used to encode private keys in a way to be more easily copied.
-					(testnet) https://github.com/litecoin-project/litecore-lib/blob/9c3b2712de14335d2a953a8772aee87e23be6cf6/lib/networks.js#L159
+// 				- Script hash:
+// 					Used in the generation of scripting addresses from public keys.
+// 					(testnet) https://github.com/litecoin-project/litecore-lib/blob/9c3b2712de14335d2a953a8772aee87e23be6cf6/lib/networks.js#L160
 
-				- BIP32 public/private key constants:
-					Used in the generation of child addresses from master public/private keys.
-					(testnet) https://github.com/litecoin-project/litecoin/blob/ba8ed3a93be7e7a97db6bc00dd7280fa2f1548bc/src/chainparams.cpp#L239-L240
-		*/
-		networks: [
-			{
-				// Pay to public key hash:
-				p2pkh: '6f',
-				// Pay to script hash:
-				p2sh: '3a',
-				bip32: {
-					public: '043587cf',
-					private: '04358394',
-				},
-			},
-			/*
-				Yes, two sets of network constants.
+// 				- WIF:
+// 					"Wallet Import Format"
+// 					Used to encode private keys in a way to be more easily copied.
+// 					(testnet) https://github.com/litecoin-project/litecore-lib/blob/9c3b2712de14335d2a953a8772aee87e23be6cf6/lib/networks.js#L159
 
-				See:
-				https://www.reddit.com/r/litecoin/comments/48wd2e/why_arent_the_litecoin_bip32_serialization_values/
-			*/
-			{
-				// Pay to public key hash:
-				p2pkh: '6f',
-				// Pay to script hash:
-				p2sh: '3a',
-				bip32: {
-					public: '0488b21e',
-					private: '0488ade4',
-				},
-			},
-		],
+// 				- BIP32 public/private key constants:
+// 					Used in the generation of child addresses from master public/private keys.
+// 					(testnet) https://github.com/litecoin-project/litecoin/blob/ba8ed3a93be7e7a97db6bc00dd7280fa2f1548bc/src/chainparams.cpp#L239-L240
+// 		*/
+// 		networks: [
+// 			{
+// 				// Pay to public key hash:
+// 				p2pkh: '6f',
+// 				// Pay to script hash:
+// 				p2sh: '3a',
+// 				bip32: {
+// 					public: '043587cf',
+// 					private: '04358394',
+// 				},
+// 			},
+// 			/*
+// 				Yes, two sets of network constants.
 
-	});
+// 				See:
+// 				https://www.reddit.com/r/litecoin/comments/48wd2e/why_arent_the_litecoin_bip32_serialization_values/
+// 			*/
+// 			{
+// 				// Pay to public key hash:
+// 				p2pkh: '6f',
+// 				// Pay to script hash:
+// 				p2sh: '3a',
+// 				bip32: {
+// 					public: '0488b21e',
+// 					private: '0488ade4',
+// 				},
+// 			},
+// 		],
 
-})();
+// 	});
+
+// })();
