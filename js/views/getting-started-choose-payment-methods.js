ab313b2d9cd5ec5655c79f61341b90404706b4f5
Carlos Garcia Ortiz <karlgoso@gmail.com>
2018-07-11 16:59:07 +0200

    choose payment methods and save it

--- js/views/getting-started-choose-payment-methods.js
+++ js/views/getting-started-choose-payment-methods.js
@@ -6,9 +6,54 @@ app.views.GettingStartedChoosePaymentMethods = (function() {
 
 	'use strict';
 
-	return app.abstracts.BaseView.extend({
+	return app.views.utility.Form.extend({
 		className: 'getting-started getting-started-choose-payment-methods',
 		template: '#template-getting-started-choose-payment-methods',
+
+		events: {
+			'change input[name="configurableCryptoCurrencies[]"]': 'onChangeConfigurableCryptocurrencies',
+		},
+
+		serializeData: function() {
+
+			var data = {};
+			var configurableCryptoCurrencies = app.settings.get('configurableCryptoCurrencies');
+			data.paymentMethods = _.map(_.keys(app.paymentMethods), function(key) {
+				var paymentMethod = _.extend(
+					{},
+					_.pick(app.paymentMethods[key], 'label', 'settings'),
+					{ key: key }
+				);
+				paymentMethod.settings = _.map(paymentMethod.settings, function(setting) {
+					return _.extend(
+						{},
+						setting,
+						{
+							id: ['settings', key, setting.name].join('-'),
+							name: [key, setting.name].join('.'),
+							value: app.settings.get(key + '.' + setting.name) || setting.default,
+						}
+					);
+				});
+				paymentMethod.accepted = _.contains(configurableCryptoCurrencies, key);
+				return paymentMethod;
+			});
+
+			return data;
+		},
+
+		onChangeConfigurableCryptocurrencies: function() {
+
+			var data = _.pick(this.getFormData(), 'configurableCryptoCurrencies');
+
+			// This is necessary to set no configurable cryptocurrencies.
+			if (_.isEmpty(data.configurableCryptoCurrencies)) {
+				data.configurableCryptoCurrencies = [];
+			}
+
+			app.settings.set(data);
+		},
+
 	});
 
 })();
