1ef88ddc33ff124ea0ed8104b3a823a8a6acebd8
Charles Hill <chill@degreesofzero.com>
2018-07-12 13:00:42 +0200

    added done to getting started

--- js/config.js
+++ js/config.js
@@ -6,6 +6,7 @@ app.config = (function() {
 
 	var config = {
 		debug: false,
+		supportEmail: 'cryptoterminal.eu@gmail.com',
 		cache: {
 			onAppStartClearOlderThan: 86400000,// milliseconds
 		},
